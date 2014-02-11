6b17d359568b436a33c7b62ea573b4a0fb70ca3e
Joel Kuzmarski <leoj3n@gmail.com>
2014-02-10 21:11:58 -0600

    New wordpress module

---
+++ modules/wordpress/init.zsh
@@ -0,0 +1,13 @@
+#
+# WordPress aliases.
+#
+# Authors:
+#   Joel Kuzmarski <leoj3n@gmail.com>
+#
+
+alias -g wpa='~/src/wp/apps'
+alias -g wpp='~/src/wp/plugs'
+alias -g apacheroots='~/Sites/www/dev/1/wp-content/themes/roots'
+
+alias skel='skelgen'
+
