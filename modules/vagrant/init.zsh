d1449ef720579b23d50ea998d0ede9e1923b7cd1
Joel Kuzmarski <leoj3n@gmail.com>
2014-02-10 21:11:44 -0600

    New vagrant module

---
+++ modules/vagrant/init.zsh
@@ -0,0 +1,16 @@
+#
+# Vagrant aliases.
+#
+# Authors:
+#   Joel Kuzmarski <leoj3n@gmail.com>
+#
+
+alias vu='vagrant up'
+alias vs='vagrant ssh'
+alias vh='vagrant halt'
+alias vr='vagrant reload'
+alias vd='vagrant destroy'
+alias vp='vagrant provision'
+alias vpu='vassh'\
+' sudo WP_TESTS_DIR="/home/vagrant/svn/wp-tests" vendor/bin/phpunit'
+
