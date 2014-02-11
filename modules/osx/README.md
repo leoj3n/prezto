dc6eb635eea5605c8c0ccbf198f7da2d4269d38e
Joel Kuzmarski <leoj3n@gmail.com>
2014-02-10 21:08:51 -0600

    Add to and remove from osx module

--- modules/osx/README.md
+++ modules/osx/README.md
@@ -10,6 +10,19 @@ Aliases
     directory.
   - `pushdf` pushes the current working directory onto the directory queue and
     changes the current working director to the current _Finder_ directory.
+  - `sson` starts the osx screen saver.
+  - `dboff` disables the dashboard.
+  - `dbon` enables the dashboard.
+  - `sloff` disables spotlight.
+  - `slon` enables spotlight.
+  - `kboff` disables the keyboard.
+  - `kbon` enables the keyboard.
+  - `emptytrash` empties the Trash on all mounted volumes and the main HDD and clears Apple’s System Logs to improve shell startup speed.
+  - `ql` quick looks at files.
+  - `rm-osx-cruft` deletes .DS\_Store, \_\_MACOSX cruft.
+  - `mvt` move files to the Mac OS X trash.
+  - `ws` search Mac OS X dictionary for matching words.
+  - `da` Dash.app lookup.
 
 Functions
 ---------
@@ -30,8 +43,10 @@ Authors
 *The authors of this module should be contacted via the [issue tracker][4].*
 
   - [Sorin Ionescu](https://github.com/sorin-ionescu)
+  - [Joel Kuzmarski](https://github.com/leoj3n)
 
 [1]: http://www.apple.com/macosx/
 [2]: http://kapeli.com/dash
 [3]: http://www.iterm2.com/
-[4]: https://github.com/sorin-ionescu/prezto/issues
+[4]: https://github.com/leoj3n/prezto/issues
+
--- modules/osx/init.zsh
+++ modules/osx/init.zsh
@@ -2,6 +2,7 @@
 # Defines Mac OS X aliases and functions.
 #
 # Authors:
+#   Joel Kuzmarski <leoj3n@gmail.com>
 #   Sorin Ionescu <sorin.ionescu@gmail.com>
 #
 
@@ -19,3 +20,57 @@ alias cdf='cd "$(pfd)"'
 
 # Pushes directory to the current Finder directory.
 alias pushdf='pushd "$(pfd)"'
+
+# Start the screen saver.
+alias sson='open "/System/Library/Frameworks/ScreenSaver'\
+'.framework/Versions/A/Resources/ScreenSaverEngine.app"'
+
+# Disable the dashboard.
+alias dboff='defaults write "com.apple.dashboard" "mcx-disabled" -boolean YES'\
+' && killall Dock'
+
+# Enable the dashboard.
+alias dbon='defaults write "com.apple.dashboard" "mcx-disabled" -boolean NO'\
+' && killall Dock'
+
+# Disable the spotlight.
+alias sloff='sudo launchctl unload -w "/System/Library/LaunchDaemons/com'\
+'.apple.metadata.mds.plist"'
+
+# Enable the spotlight.
+alias slon='sudo launchctl load -w "/System/Library/LaunchDaemons/com'\
+'.apple.metadata.mds.plist"'
+
+# Disable the keyboard.
+alias kboff='sudo kextunload "/System/Library/Extensions/AppleUSBTopCase'\
+'.kext/Contents/PlugIns/AppleUSBTCKeyboard.kext" > /dev/null 2>&1'
+
+# Enable the keyboard.
+alias kbon='sudo kextload "/System/Library/Extensions/AppleUSBTopCase'\
+'.kext/Contents/PlugIns/AppleUSBTCKeyboard.kext"'
+
+# Empty the Trash on all mounted volumes and the main HDD.
+# Also, clear Apple’s System Logs to improve shell startup speed.
+alias emptytrash='sudo rm -rfv /Volumes/*/.Trashes;'\
+'sudo rm -rfv ~/.Trash/*;'\
+'sudo rm -rfv /private/var/log/asl/*.asl'
+
+#
+# Functions
+#
+
+# Move files to the Mac OS X trash.
+function mvt() {
+  mv "$@" ~'/.Trash'
+}
+
+# Search Mac OS X dictionary for matching words.
+function ws() {
+  grep --color -i "$1" '/usr/share/dict/words'
+}
+
+# Dash.app lookup.
+function da() {
+  o "dash://$1"
+}
+
