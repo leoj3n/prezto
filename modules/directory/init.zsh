95638f2185307c41ea6cdfc21ac988192bb64801
Joel Kuzmarski <leoj3n@gmail.com>
2015-01-22 05:31:00 -0600

    Remove unused code from directory module

--- modules/directory/init.zsh
+++ modules/directory/init.zsh
@@ -30,7 +30,6 @@ unsetopt CLOBBER            # Do not overwrite existing files with > and >>.
 alias d='dirs -v'
 for index ({1..9}) alias "$index"="cd +${index}"; unset index
 
-alias -- -='cd -' # FIXME: Not working in zsh prezto
 alias cdd='cd -'
 alias ...='cd ../..'
 
